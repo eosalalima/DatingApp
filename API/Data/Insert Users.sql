e43189fbb567655a788a25a1604e92a714566f10
eric_salalima <eosalalima@gmail.com>
2025-11-03 14:36:52 +0800

    Set up MembersController and initial database table User

---
+++ API/Data/Insert Users.sql
@@ -0,0 +1,10 @@
+-- Active: 1759150003540@@db.znrrgffmfdfzihmxcohd.supabase.co@5432
+-- SQLite
+INSERT INTO Users (Id, DisplayName, Email)
+VALUES ('bob-id', 'Bob', 'bob@test.com');
+
+INSERT INTO Users (Id, DisplayName, Email)
+VALUES ('tom-id', 'Tom', 'tom@test.com');
+
+INSERT INTO Users (Id, DisplayName, Email)
+VALUES ('jane-id', 'Jane', 'jane@test.com');
